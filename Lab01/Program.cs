c8307ef05829d52ca601a432cdb096d990892f64
whypikmi <ttelatnikova1@yandex.ru>
2026-09-08 14:49:05 +0300

    Lab01: arithmetic with variables

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine();
+Console.WriteLine("Ремонт: комната");
+
+double roomWidth = 3.5;
+double roomLength = 4.2;
+
+double roomArea = roomLength * roomWidth;
+double roomPerimeter = (roomWidth + roomLength) * 2;
+
+Console.WriteLine($"Ширина: {roomWidth} м, длина: {roomLength} м");
+Console.WriteLine($"Площадь: {roomArea} кв.м");
+Console.WriteLine($"Периметр: {roomPerimeter} м");
+
+Console.WriteLine();
+Console.WriteLine("Покупка ноутбука в рассрочку");
+
+int Price = 65000;
+int Count = 12;
+double Rate = 0.08;
+
+double total = Price * (1 + Rate);
+double Payment = total / Count;
+
+Console.WriteLine($"Цена ноутбука:{Price} руб.");
+Console.WriteLine($"Итого с процентами: {total} руб.");
+Console.WriteLine($"Платёж в месяц: {Payment} руб.");
+
+Console.WriteLine("");
+int a = 25;
+int b = 4;
+int c = a / b;
+double f = (double)a/b;
+Console.WriteLine($"int{c}");
+Console.WriteLine($"double{f}");

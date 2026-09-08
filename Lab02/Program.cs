a5b3df99a938da7fc8b1387e22bbcd0e20a4e7c9
whypikmi <ttelatnikova1@yandex.ru>
2026-09-08 15:54:44 +0300

    Lab02: numeric type ranges and overflow

---
+++ Lab02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine($"byte: {byte.MinValue} .. {byte.MaxValue}");
+Console.WriteLine($"short: {short.MinValue} .. {short.MaxValue}");
+Console.WriteLine($"int: {int.MinValue} .. {int.MaxValue}");
+Console.WriteLine($"long: {long.MinValue} .. {long.MaxValue}");
+
+Console.WriteLine("");
+Console.WriteLine($"float: {float.MinValue} .. {float.MaxValue}");
+Console.WriteLine($"double: {double.MinValue} .. {double.MaxValue}");
+Console.WriteLine($"decimal: {decimal.MinValue} .. {decimal.MaxValue}");
+
+byte maxByte = 255;
+byte over = (byte)(maxByte + 1);
+Console.WriteLine(over);
+

59f0514de183dbb507bb3f9e0a9821400780d13e
Bebra1-7 <grigorievn603@gmail.com>
2026-09-15 10:33:55 +0300

    Lab04_If_Else

---
+++ Lab04_Grigoriev/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+int score = 1200;
+if (score >= 500)
+{
+    Console.WriteLine("Новичек");//Можно убрать {}, если только один блок кода
+
+}
+else if (score >= 1000)
+{
+    Console.WriteLine("Боец");
+}
+else if (score >= 3000)
+{
+    Console.WriteLine("Мутант");
+}
+// else
+// {
+//     Console.WriteLine("Доступ закрыт");
+// }
+
+Console.WriteLine("Игра продолжается");
+

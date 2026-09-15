1662e68c85b4c65a1a30a5ba4066ec9e56255caf
Bebra1-7 <grigorievn603@gmail.com>
2026-09-15 10:45:50 +0300

    Lab04_Tir_Operator

--- Lab04_Grigoriev/Program.cs
+++ Lab04_Grigoriev/Program.cs
@@ -1,23 +1,52 @@
-﻿using System.Threading.Tasks.Dataflow;
+﻿// If_Else
+// int score = 1200;
+// if (score >= 500)
+// {
+//     Console.WriteLine("Новичек");//Можно убрать {}, если только один блок кода
 
-int score = 1200;
-if (score >= 500)
-{
-    Console.WriteLine("Новичек");//Можно убрать {}, если только один блок кода
-
-}
-else if (score >= 1000)
-{
-    Console.WriteLine("Боец");
-}
-else if (score >= 3000)
-{
-    Console.WriteLine("Мутант");
-}
+// }
+// else if (score >= 1000)
+// {
+//     Console.WriteLine("Боец");
+// }
+// else if (score >= 3000)
+// {
+//     Console.WriteLine("Мутант");
+// }
 // else
 // {
 //     Console.WriteLine("Доступ закрыт");
 // }
 
-Console.WriteLine("Игра продолжается");
+// Console.WriteLine("Игра продолжается");
 
+// int movieNumber = int.Parse(Console.ReadLine());
+
+// string movieGenre;
+
+// switch (movieNumber)
+// {
+//     case 1:
+//         movieGenre = "Боевик";
+//         break;
+//     case 2:
+//         movieGenre = "Комедия";
+//         break;
+//     default:
+//         movieGenre = "Неизвест. жанр";
+//         break;
+// }
+// Console.WriteLine(movieNumber);
+
+// int score = 3000;
+// int recordScore = 3000;
+// string result = (score > recordScore) ? "New record" : "Common result";//Тирнарный оператор
+// if (score > recordScore)
+// {
+//     result = "New record";
+// }
+// else
+// {
+//     result = "Common result";
+// }
+// Console.WriteLine(result);
